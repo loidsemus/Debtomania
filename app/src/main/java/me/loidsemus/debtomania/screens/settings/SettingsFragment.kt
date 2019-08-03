4f91a3f591cd7d06f20e88482e8fc7228faf7e0b
Emil <emil.j.carlen@gmail.com>
2019-08-03 21:29:14 +0200

    Make dark mode apply instantly without restart

--- app/src/main/java/me/loidsemus/debtomania/screens/settings/SettingsFragment.kt
+++ app/src/main/java/me/loidsemus/debtomania/screens/settings/SettingsFragment.kt
@@ -1,13 +1,38 @@
 package me.loidsemus.debtomania.screens.settings
 
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.preference.PreferenceFragmentCompat
 import me.loidsemus.debtomania.R
 
-class SettingsFragment : PreferenceFragmentCompat() {
+
+class SettingsFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.prefs, rootKey)
+
+
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+        if(key == "dark_mode") {
+            activity?.let {
+                it.intent?.action = Intent.ACTION_MAIN
+                it.recreate()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        preferenceManager.sharedPreferences.registerOnSharedPreferenceChangeListener(this)
+
+    }
+
+    override fun onPause() {
+        preferenceManager.sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
+        super.onPause()
     }
 
 }
