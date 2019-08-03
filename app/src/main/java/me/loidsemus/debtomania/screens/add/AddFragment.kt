5abacfccf28b79b25ea5a9abc24f2ae739a0f191
Emil <emil.j.carlen@gmail.com>
2019-08-03 21:51:53 +0200

    Added form validation

--- app/src/main/java/me/loidsemus/debtomania/screens/add/AddFragment.kt
+++ app/src/main/java/me/loidsemus/debtomania/screens/add/AddFragment.kt
@@ -8,7 +8,9 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.add_fragment.*
 import kotlinx.android.synthetic.main.add_fragment.view.*
+import kotlinx.android.synthetic.main.add_fragment.view.amountInput
 import me.loidsemus.debtomania.R
 import me.loidsemus.debtomania.SharedViewModel
 import me.loidsemus.debtomania.database.Debt
@@ -29,16 +31,40 @@ class AddFragment : Fragment() {
         binding.viewModel = viewModel
 
         binding.root.doneButton.setOnClickListener {
-            val debt = Debt(
-                0,
-                binding.root.amountInput.text.toString().toDouble(),
-                binding.root.nameInput.text.toString(),
-                System.currentTimeMillis()
-            )
-            viewModel.insert(debt)
-            findNavController().navigateUp()
+            if (validate()) {
+                val debt = Debt(
+                    0,
+                    binding.root.amountInput.text.toString().toDouble(),
+                    binding.root.nameInput.text.toString(),
+                    System.currentTimeMillis()
+                )
+                viewModel.insert(debt)
+                findNavController().navigateUp()
+            }
         }
 
         return binding.root
+    }
+
+    private fun validate(): Boolean {
+        var valid = false
+
+        if (amountInput.text.toString() == "") {
+            amountInputLayout.error = "You need to specify an amount"
+        } else {
+            amountInputLayout.error = null
+        }
+
+        if (nameInput.text.toString() == "") {
+            nameInputLayout.error = "You need to specify a name"
+        } else {
+            nameInputLayout.error = null
+        }
+
+        if (amountInput.text.toString() != "" && nameInput.text.toString() != "") {
+            valid = true
+        }
+
+        return valid
     }
 }
